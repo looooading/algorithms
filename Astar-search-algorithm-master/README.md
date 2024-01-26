5721b134560b053aa2068554a606bd655fb62a42
looooading <1275318444@qq.com>
2024-01-26 23:52:47 +0800

    初始化素材库，收集了骨干算法包括：A星算法、混合A星算法与狼群算法

---
+++ Astar-search-algorithm-master/README.md
@@ -0,0 +1,12 @@
+# Astar-search-algorithm
+search the shortest road in an image by A* algorithm
+
+If we have a map in the form of image or 2D-array and we want to search the shortese road,
+one of the based way is A*, although we need a precondition is that we have abstract our map to many nodes.
+but if we view every pixel as a nodes , A* still could be used straightly.
+
+This code will read a image:"maps.png". in this image,  zero gray value represent this pixel is a barrier.
+Our result will be save as a image "informap.png". I have drawn the road in it.
+
+![Alt text](https://github.com/yimingstyle/Astar-search-algorithm/blob/master/informap.png)
+
---
+++ WPA/wolf_pack_algorithm.m
@@ -0,0 +1,75 @@
+% 狼群算法
+function [bestX, bestF] = wolf_pack_algorithm(fitness_func, lb, ub, dim, max_gen, pop_size, alpha, beta, delta)
+% fitness_func - 适应度函数
+% lb - 自变量下界
+% ub - 自变量上界
+% dim - 自变量维度
+% max_gen - 最大迭代次数
+% pop_size - 种群大小
+% alpha - 狼群更新常数
+% beta - 狼群更新常数
+% delta - 狼群更新常数
+    % 初始化种群
+    pop = create_population(lb, ub, dim, pop_size);
+    % 计算适应度
+    fitness = evaluate_fitness(fitness_func, pop, pop_size);
+    % 寻找最优解
+    [bestF, bestIdx] = min(fitness);
+    bestX = pop(bestIdx, :);
+    % 迭代优化
+    for gen = 1:max_gen
+        % 更新狼群位置
+        for i = 1:pop_size
+            % 计算狼群中每个狼的适应度
+            fitness_i = fitness(i);
+            for j = 1:pop_size
+                if fitness(j) < fitness_i
+                    r1 = rand(1, dim);
+                    r2 = rand(1, dim);
+                    A = alpha * (2 * r1 - 1);
+                    C = 2 * r2;
+                    D = abs(C .* bestX - pop(i, :));
+                    X1 = bestX - A .* D;
+                    fitness_X1 = evaluate_fitness(fitness_func, X1, 1);
+                    % 更新最优解
+                    if fitness_X1 < bestF
+                        bestF = fitness_X1;
+                        bestX = X1;
+                    end
+                    % 更新狼群位置
+                    if fitness_X1 < fitness_i
+                        pop(i, :) = X1;
+                        fitness_i = fitness_X1;
+                    else
+                        r3 = rand;
+                        if r3 < beta
+                            X2 = pop(j, :) + delta * (rand(1, dim) - 0.5);
+                            fitness_X2 = evaluate_fitness(fitness_func, X2, 1);
+                            % 更新最优解
+                            if fitness_X2 < bestF
+                                bestF = fitness_X2;
+                                bestX = X2;
+                            end
+                            % 更新狼群位置
+                            if fitness_X2 < fitness_i
+                                pop(i, :) = X2;
+                                fitness_i = fitness_X2;
+                            end
+                        end
+                    end
+                end
+            end
+        end
+    end
+end
+% 初始化种群
+function pop = create_population(lb, ub, dim, pop_size)
+    pop = repmat(lb, pop_size, 1) + rand(pop_size, dim) .* repmat((ub - lb), pop_size, 1);
+end
+% 计算适应度
+function fitness = evaluate_fitness(fitness_func, pop, pop_size)
+    fitness = zeros(pop_size, 1);
+    for i = 1:pop_size
+        fitness(i) = fitness_func(pop(i, :));
+    end
+end
